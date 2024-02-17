cf11eabf9aeae8b5fad29ca78fa972153e426dca
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-02-18 00:27:21 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (48.59%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,38 +1,18 @@
-import java.util.*;
-class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int max=0;
-        for(int i[]:accounts)
-        { 
-            int m=0;
-            for(int j:i) 
-            {
-              m=m+j;  
-            }
-            if(max<m)
-            {
-                max=m;
-            }
-        }
-        return max;
-        
-    }
-    public static void main(String args[])
+class Solution 
+{
+    public int maximumWealth(int[][] accounts)
     {
-        Scanner sc = new Scanner(System.in);
-        int m=sc.nextInt();
-        int n=sc.nextInt();
-        int a[][]=new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++) 
-            {
-                a[i][j]=sc.nextInt();
-            }
-        }
-        Solution s =  new Solution();
-        System.out.println(s.maximumWealth(a));
-        
+        int mx=0;
+        int c=0;
+       for(int i=0;i<accounts.length;i++)
+           {
+           c=0;
+                for(int j=0;j<accounts[i].length;j++)
+                {
+                    c=accounts[i][j]+c;
+                }
+           mx=Math.max(c,mx);
+           }
+        return mx;
     }
-        
 }
