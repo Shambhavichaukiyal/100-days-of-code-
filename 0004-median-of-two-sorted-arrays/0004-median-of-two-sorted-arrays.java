47fe53e768297601e5a2e503bc32fd1476a2091f
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-02-20 02:22:43 +0530

    Time: 1 ms (100.00%), Space: 45.9 MB (62.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,32 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int o=nums1.length+nums2.length;
-        int a[]=new int[o];
-         int k=0;
-            double median;
-        for(int i=0;i<nums1.length;i++)
-        {
-            a[i]=nums1[i];
+        int i = 0, j = 0, k = 0;
+        int[] mergedArray = new int[nums1.length + nums2.length];
+        
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                mergedArray[k++] = nums1[i++];
+            } else {
+                mergedArray[k++] = nums2[j++];
+            }
         }
-             for(int j=nums1.length;j<a.length;j++)
-             {
-                
-                 a[j]=nums2[k];
-                 k++;
-                 
-}
-        Arrays.sort(a);
-        int med;
-int z=a.length/2;
-        if(a.length%2==0)
-        {
-            med=(a[z]+(a[z-1]));
-            median=med/2.0;
-}
-        else
-        {
-            median=a[z];
+        
+      
+        while (i < nums1.length) {
+            mergedArray[k++] = nums1[i++];
         }
-        return median;
+        
+        
+        while (j < nums2.length) {
+            mergedArray[k++] = nums2[j++];
+        }
+        
+        int n = mergedArray.length;
+        int mid = n / 2;
+        
+        if (n % 2 == 0) {
+            // Even number of elements, return average of middle two elements
+            return (mergedArray[mid - 1] + mergedArray[mid]) / 2.0;
+        } else {
+            // Odd number of elements, return middle element
+            return mergedArray[mid];
+        }
     }
 }
