6555d1d23a94ada37af9e0e3ee069bd997da2779
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-02-19 20:32:18 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (72.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,33 +1,47 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int a = -1; // Initialize a and c with -1 to indicate target not found
-        int c = -1;
-        int[] b = new int[2];
-        int[] o = {-1, -1};
-        int y = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                a = i;
-                b[0] = a;
-                y = 1;
-                break;
-            }
+    public int[] searchRange(int[] nums, int x) {
+        int result[]={-1,-1};
+         int l = 0;
+        int r = nums.length - 1; 
+        int mid = 0;
+        
+    while(l<=r)
+    {
+        mid=l+(r-l)/2;
+        if(nums[mid]>x)
+        {
+          r=mid-1;  
         }
-
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] == target) {
-                c = i;
-                b[1] = c;
-                y = 1;
-                break;
-            }
+        else if(nums[mid]<x)
+        {
+           l=mid+1; 
         }
-
-        if (y == 1) {
-            return b;
-        } else {
-            return o;
+        else
+        {
+            result[0]=mid;
+            r=mid-1;
+        }}
+        l = 0;
+         r = nums.length - 1; 
+         mid = 0;
+     while(l<=r)
+    {
+         mid=l+(r-l)/2;
+        if(nums[mid]>x)
+        {
+          r=mid-1;  
+        }
+        else if(nums[mid]<x)
+        {
+           l=mid+1; 
+        }
+        else
+        {
+            result[1]=mid;
+            l=mid+1;
         }
     }
-}
+        return result;
+    
+}}
+   
