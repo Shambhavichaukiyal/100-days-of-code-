7564bf7dbfc26587b6f7b9381b7f85cd65ccdef7
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-02-28 23:33:54 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (47.40%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) 
+    {
+        char[] a = jewels.toCharArray();
+        char[] b = stones.toCharArray();
+        int c = 0;
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = 0; j < b.length; j++)
+            {
+                if (a[i] == b[j])
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
