1afc4a65fb6eba420862718a1d2cb9a5e9bd418e
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-06-02 22:08:40 +0530

    Time: 14 ms (65.40%), Space: 54.2 MB (24.71%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,27 @@
+class MyHashSet {
+private Map<Integer,Boolean> map;
+    public MyHashSet() {
+        map= new HashMap<>();
+    }
+    
+    public void add(int key) {
+        map.put(key,true);
+    }
+    
+    public void remove(int key) {
+        map.remove(key);
+    }
+    
+    public boolean contains(int key) 
+    {
+       return  map.containsKey(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
