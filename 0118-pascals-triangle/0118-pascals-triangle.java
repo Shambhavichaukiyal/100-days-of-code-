355bfe51ae9ae4bbe24ac22d54cf2a38b1ec646f
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-09-05 01:29:55 +0530

    Time: 1 ms (82.58%), Space: 41.9 MB (65.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,18 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> wrap= new ArrayList<>();
+        for(int i=1;i<=numRows;i++)
+        {
+         List<Integer> a = new ArrayList<>();
+            for(int j=1;j<=i;j++)
+            {
+                a.add(nCr(i-1,j-1));
+            }
+            wrap.add(a);
+        }
+        return wrap;
+    }
+    public static int nCr(int n,int r)
         
-    List<List<Integer>> ans = new ArrayList<>();
-
-    for (int i = 0; i < numRows; ++i) {
-      Integer[] temp = new Integer[i + 1];
-      Arrays.fill(temp, 1);
-      ans.add(Arrays.asList(temp));
-    }
-
-    for (int i = 2; i < numRows; ++i)
-      for (int j = 1; j < ans.get(i).size() - 1; ++j)
-        ans.get(i).set(j, ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
-
-    return ans;
-    }
-}
+    {
+        long res=1;
+        for(int i=0;i<r;i++)
+        {
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        return (int)res;
+    }}
