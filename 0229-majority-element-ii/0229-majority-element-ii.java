8a2814344526e389ec3fccd48600a140680a3f57
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-09-13 13:16:46 +0530

    Time: 2 ms (99.50%), Space: 48.4 MB (12.45%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,39 +1,64 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-       List<Integer> result = new ArrayList<>();
-        if (nums.length == 0) return result;
-
-        int count1 = 0, count2 = 0;
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
-
-    
-        for (int num : nums) {
-            if (num == ele1) {
+        
+        List<Integer> result= new ArrayList<>();
+        if(nums.length==0)
+        {
+            
+            
+            return result;
+        }
+        int count1=0;
+        int count2=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==ele1)
+            {
                 count1++;
-            } else if (num == ele2) {
+            }
+            else if(nums[i]==ele2)
+            {
                 count2++;
-            } else if (count1 == 0) {
-                ele1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                ele2 = num;
-                count2 = 1;
-            } else {
+            }
+            else if(count1==0)
+            {
+                count1++;
+                ele1=nums[i];
+            }
+            else if(count2==0)
+            {
+                count2++;
+                ele2=nums[i];
+            }
+            else 
+            {
                 count1--;
                 count2--;
             }
         }
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == ele1) count1++;
-            else if (num == ele2) count2++;
+         count1=0;
+         count2=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==ele1)
+            {
+                count1++;
+            }
+            else if(nums[i]==ele2)
+            {
+                count2++;
+            }
         }
-
-        
-        if (count1 > nums.length / 3) result.add(ele1);
-        if (count2 > nums.length / 3) result.add(ele2);
-
+        if(count1>nums.length/3)
+        {
+            result.add(ele1);
+        }
+        if(count2>nums.length/3)
+        {
+            result.add(ele2);
+        }
         return result;
     }
 }
