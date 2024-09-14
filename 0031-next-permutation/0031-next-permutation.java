b9ef4474020ad55c7b9239ebad6f73224f0a0d73
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-09-14 23:31:07 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (77.74%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,12 +1,12 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
         int index=-1;
+        int n=nums.length;
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
             {
-             index=i;
+                index=i;
                 break;
             }
         }
@@ -14,22 +14,22 @@ class Solution {
         {
             rev(nums,0,n-1);
         }
-      else
-      {
-          for(int i= n-1;i>=0;i--)
-          {
-              if(nums[i]>nums[index])
-              {
-                  int temp=nums[i];
-                  nums[i]=nums[index];
-                  nums[index]=temp;
-                  break;
-              }
-          }
-          rev(nums,index+1,n-1);
-      }
+        else
+        {
+            for (int j = n - 1; j > index; j--) {
+                if (nums[j] > nums[index]) {
+                    
+                    int temp = nums[index];
+                    nums[index] = nums[j];
+                    nums[j] = temp;
+                    break;
+                }
+            }
+            rev(nums, index + 1, n - 1);
+        }
+        
     }
-public static void rev(int nums[],int l,int r)
+public void rev(int nums[],int l,int r)
 {
     while(l<=r)
     {
@@ -40,4 +40,5 @@ public static void rev(int nums[],int l,int r)
         r--;
     }
 }
+   
 }
