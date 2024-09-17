8f5d3bbe301c94ee1c3f4780987b71b7639b71b2
Shambhavichaukiyal <98662147+Shambhavichaukiyal@users.noreply.github.com>
2024-09-17 10:29:14 +0530

    Time: 2 ms (52.24%), Space: 41.8 MB (16.64%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,26 @@
-class Solution 
-{
-    public boolean isValid(String s) 
-    {
-       char chars[] = s.toCharArray();
-        Stack<Character> stack= new Stack<>();
-        for(char ele:chars)
-        {
-            if(ele=='(' || ele=='{'||ele=='[')
-            {
-                stack.push(ele);
-            }
-            else
-            {
-                if(stack.empty())
-                {
-                    return false;
-                }
-            
-            char top=stack.pop();
-             if ((top == '[' && ele != ']') || (top == '(' && ele != ')') || (top == '{' && ele != '}')) {
-                    return false; 
-                }
-        }}
-        return stack.empty();
-    }}
+class Solution {
+    public boolean isValid(String s) {
         
-   
+        Stack<Character> sta = new Stack<>();
+for (int i = 0; i < s.length(); i++) {
+    char ch = s.charAt(i);
+
+    if (ch == '(' || ch == '[' || ch == '{') {
+        sta.push(ch);
+    } 
+
+    else if (!sta.isEmpty() && 
+            (sta.peek() == '(' && ch == ')' || 
+             sta.peek() == '[' && ch == ']' || 
+             sta.peek() == '{' && ch == '}')) {
+        sta.pop();
+    } 
+    
+    else {
+        return false;
+    }
+}
+
+
+return sta.isEmpty();
+    }}
